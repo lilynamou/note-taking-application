f2d55d6c7c9a06757620eda75bc05b3eed175411
lilynamou <lilyananamou@gmail.com>
2022-04-29 00:53:19 -0700

    server.js updated

--- db/server.js
+++ db/server.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
-const util = require("util");
+const uuid = require("uuid");
+const notes = require("./db/db.json");
+const {DH_CHECK_P_NOT_SAFE_PRIME} = require("constants");
 
 const PORT = process.env.PORT || 3001;
 
@@ -12,7 +14,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 
-app.use(express.static('public'));
+app.use(express.static('./public'));
+
+//Get API db.json
+app.get("/api/notes", (req, res) => {
+    res.sendFile(path.join(_dirname, "./db/db.json"))
+});
 
 //Add notes to db.json
 app.post("/api/notes", (req,res) => {
@@ -24,16 +31,18 @@ app.post("/api/notes", (req,res) => {
     res.json(notes);
 });
 
-//Get API db.json
-app.get("/api/notes", (req, res) => {
-    res.sendFile(path.join(_dirname, "./db/db.json"))
-});
 
 // GET Route for homepage
-app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
+app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/index.html"))
 );
 
+//notes.html
+app.get("/notes",(req, res) =>
+  res.sendFile(path.join(__dirname, "./public/notes.html"))
+);
+
+//Listen
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
